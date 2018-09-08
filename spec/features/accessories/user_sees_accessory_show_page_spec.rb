aabcd9d90b8e9654e27382aef29bf785a76df63f
andrewetobin <andrewetobin@gmail.com>
2018-09-08 13:41:18 -0600

    adds spec for retired accessory

--- spec/features/accessories/user_sees_accessory_show_page_spec.rb
+++ spec/features/accessories/user_sees_accessory_show_page_spec.rb
@@ -11,4 +11,16 @@ describe 'user visits accessory show page' do
     expect(page).to have_content("Price: #{accessory_1.price}")
     expect(page).to have_button("Add to Cart")
   end
+  it "shows a retired accessory correctly" do
+    accessory_1 = Accessory.create(title: 'Gold Pedal', description: 'for added performance and beautiful bling bling', price: 900.00, retired?: true)
+
+    visit accessory_show_path(accessory_1)
+
+    expect(page).to have_content(accessory_1.title)
+    expect(page).to have_content(accessory_1.description)
+    expect(page).to have_content("Price: #{accessory_1.price}")
+    expect(page).to_not have_button("Add to Cart")
+    expect(page).to have_button("Retired")
+
+  end
 end
