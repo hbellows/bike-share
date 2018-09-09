18c45794b049966dd53659db9abcc0580c3ae977
abdyqew <aqudrat630@gmail,com>
2018-09-09 11:57:29 -0600

    adds condition index/show route/controller/view tests pass, moves spec files

--- spec/features/conditions/condition_show_spec.rb
+++ spec/features/conditions/condition_show_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 feature 'Condition show page' do
-  scenario 'when a user visits a condition show page' do
+  context 'when a user visits a condition show page' do
     it 'shows all condition information' do
-      condition = Condition.create(date: '8/29/2013', max_temperature_f: 99.0,
-        mean_temperature_f: 75.0, min_temperature_f: 60.0, mean_humidity: 75.0,
-        mean_visibility_miles: 10.0, mean_wind_speed_mph: 11.0,
-        precipitation_inches: 0.01)
 
-        visit condition_path(condition) #?
+    condition = Condition.create(
+      date: Date.new(2018, 4, 29),
+      max_temperature: 75.0,
+      mean_temperature: 65.0,
+      min_temperature: 55.0,
+      mean_humidity: 30.0,
+      mean_visibility: 15.0,
+      mean_wind_speed: 9.0,
+      precipitation: 0.01)
 
-        expec(page).to have_content(condition.date)
-        expec(page).to have_content(condition.max_temperature_f)
-        expec(page).to have_content(condition.mean_temperature_f)
-        expec(page).to have_content(condition.min_temperature_f)
-        expec(page).to have_content(condition.mean_humidity)
-        expec(page).to have_content(condition.mean_visibility)
-        expec(page).to have_content(condition.mean_wind_speed)
+      visit condition_path(condition)
+
+      expect(page).to have_content(condition.date.strftime('%m/%d/%Y'))
+      expect(page).to have_content(condition.max_temperature)
+      expect(page).to have_content(condition.mean_temperature)
+      expect(page).to have_content(condition.min_temperature)
+      expect(page).to have_content(condition.mean_humidity)
+      expect(page).to have_content(condition.mean_visibility)
+      expect(page).to have_content(condition.mean_wind_speed)
     end
   end
 end
