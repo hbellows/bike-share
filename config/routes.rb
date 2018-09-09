936a1aa2b5f5ff9324404c6bfc7da708922e107a
abdyqew <aqudrat630@gmail,com>
2018-09-09 15:21:23 -0600

    adds dashboard route/controller/view, adds session routes/controller/view, adds user routes/controller/view, adds layout auth stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :stations, only: [:index]
+  root to: 'dashboard#index'
 
-  get '/:id', to: 'stations#show'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show]
+
+  resources :stations, only: [:index]
 
   resources :trips, only: [:index, :show]
 
   resources :conditions, only: [:index, :show]
+
+  get '/:id', to: 'stations#show'
 end
--- spec/features/users/authenticate_new_user_spec.rb
+++ spec/features/users/authenticate_new_user_spec.rb
@@ -2,27 +2,30 @@ require 'rails_helper'
 
 describe 'registration and login' do
   describe 'registration' do
-    username = 'bikeshareuser'
+    it 'new user should be able to register' do
+      username = 'bikeshareuser'
 
-    visit root_path
+      visit root_path
 
-    click_on 'Login'
+      click_on 'Login'
 
-    expect(current_path).to eq(login_path)
+      expect(current_path).to eq(login_path)
 
-    click_on "Create Account"
+      click_on "Create Account"
 
-    expect(current_path).to eq(new_user_path)
+      expect(current_path).to eq(new_user_path)
 
-    fill_in :username, with: username
-    fill_in :password, with: 'test'
-    fill_in :password_confirmation, with: 'test'
+      fill_in :user_email, with: "xyz@gmale.com"
+      fill_in :user_username, with: username
+      fill_in :user_password, with: 'test'
+      fill_in :user_password_confirmation, with: 'test'
 
-    click_on "Create User"
+      click_on "Create User"
 
-    expect(page).to have_content("Logged in as #{username}!")
-    expect(page).to have_content("#{username}'s Dashboard")
-    expect(page).to_not have_content('Login')
-    expect(page).to have_content('Logout')
+      expect(page).to have_content("Logged in as #{username}!")
+      expect(page).to have_content("#{username}'s Dashboard")
+      expect(page).to_not have_content('Login')
+      expect(page).to have_content('Logout')
+    end
   end
 end
