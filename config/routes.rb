8ec49f0834fcf87f799800ca5707f93584ee5026
abdyqew <aqudrat630@gmail,com>
2018-09-09 12:47:13 -0600

    adds station show route/controller/view, adds slug gem and slug implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stations, only: [:index]
+  get '/:id', to: 'stations#show'
   resources :conditions, only: [:index, :show]
 end
