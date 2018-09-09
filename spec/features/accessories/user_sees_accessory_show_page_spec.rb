4a129636faf9e52b9151db8f6da02e05d9354f00
andrewetobin <andrewetobin@gmail.com>
2018-09-09 16:09:43 -0600

    passes retired button test

--- spec/features/accessories/user_sees_accessory_show_page_spec.rb
+++ spec/features/accessories/user_sees_accessory_show_page_spec.rb
@@ -5,17 +5,17 @@ describe 'user visits accessory show page' do
     accessory_1 = Accessory.create(name: 'Gold Pedal', description: 'for added performance and beautiful bling bling', price: 900.00)
 
     visit accessory_path(accessory_1)
-save_and_open_page
+
     expect(page).to have_content(accessory_1.name)
     expect(page).to have_content(accessory_1.description)
     expect(page).to have_content("Price: $900")
     expect(page).to have_button("Add to Cart")
   end
-  xit "shows a retired accessory correctly" do
+  it "shows a retired accessory correctly" do
     accessory_1 = Accessory.create(name: 'Gold Pedal', description: 'for added performance and beautiful bling bling', price: 900.00, retired?: true)
 
     visit accessory_path(accessory_1)
-
+save_and_open_page
     expect(page).to have_content(accessory_1.name)
     expect(page).to have_content(accessory_1.description)
     expect(page).to have_content("Price: $900.00")
