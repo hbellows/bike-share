5f2f34a5e7e26efff27e5a5e1dba103a40600f54
Mike McKee <mikecm@gmail.com>
2018-09-11 16:48:02 -0600

    Refactor order accessory model tests

--- spec/models/order_accessory_spec.rb
+++ spec/models/order_accessory_spec.rb
@@ -7,21 +7,17 @@ describe OrderAccessory, type: :model do
   end
 
   describe 'Instance Methods' do
+    before(:each) do
+      @user = create(:user)
+      @accessory = create(:accessory)
+      @order = @user.orders.create(status: 'paid')
+      @order_accessory = @order.order_accessories.create(accessory: @accessory, quantity: 2, unit_price: @accessory.price)
+    end
     it "#accessory_name" do
-      user = create(:user)
-      accessory = create(:accessory)
-      order = user.orders.create(status: 'paid')
-      order_accessory = order.order_accessories.create(accessory: accessory, quantity: 2, unit_price: accessory.price)
-
-      expect(order_accessory.accessory_name).to eq(accessory.name)
+      expect(@order_accessory.accessory_name).to eq(@accessory.name)
     end
     it "#subtotal" do
-      user = create(:user)
-      accessory = create(:accessory)
-      order = user.orders.create(status: 'paid')
-      order_accessory = order.order_accessories.create(accessory: accessory, quantity: 2, unit_price: accessory.price)
-
-      expect(order_accessory.subtotal).to eq(order_accessory.quantity * order_accessory.unit_price)
+      expect(@order_accessory.subtotal).to eq(@order_accessory.quantity * @order_accessory.unit_price)
     end
   end
 end
