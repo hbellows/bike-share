48277cef9bf6dbaf3115c29d990ddf0e7766fd23
Mike McKee <mikecm@gmail.com>
2018-09-11 15:39:09 -0600

    Fixing paths in tests from user_path changing to dashboard_path

--- spec/features/users/user_authorization_spec.rb
+++ spec/features/users/user_authorization_spec.rb
@@ -12,23 +12,12 @@ feature 'User authorization' do
   end
     describe 'as a logged in user, when I visit any administrator page' do
       it 'redirects to /login and gives me an error message' do
-        user = create(:user, password: 'test')
+        user = create(:user)
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
         visit admin_stations_path
 
         expect(page).to have_content("The page you were looking for doesn't exist.")
-      end
-    end
-    describe "as a logged in user" do
-      it 'prevents me from seeing another users private details' do
-        user1 = create(:user, password: 'test')
-        user2 = create(:user, password: 'test2')
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-
-        visit user_path(user2)
-
-        expect(current_path).to eq(user_path(user1))
       end
     end
   end
