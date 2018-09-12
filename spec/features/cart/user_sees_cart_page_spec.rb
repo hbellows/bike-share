f2302f53c91dcc9002cfe0c703b7829d2df3cd96
abdyqew <aqudrat630@gmail,com>
2018-09-12 12:35:36 -0600

    adds checkout cart functionality, ATP

--- spec/features/cart/user_sees_cart_page_spec.rb
+++ spec/features/cart/user_sees_cart_page_spec.rb
@@ -4,7 +4,7 @@ describe "user visits their cart page" do
   before(:each) do
     @user = create(:user)
     @accessory_1 = create(:accessory, price: 25)
-    @accessory_12 = create(:accessory, price: 15)
+    @accessory_2 = create(:accessory, price: 15)
   end
 
   it "shows all accessories that have been added to cart" do
@@ -16,27 +16,27 @@ describe "user visits their cart page" do
     end
 
     2.times do
-      within("#accessory-#{@accessory_12.id}") do
+      within("#accessory-#{@accessory_2.id}") do
         click_button "Add to Cart"
       end
     end
 
     visit '/cart'
 
-    subtotal_12 = (@accessory_12.price * 2)
+    subtotal_2 = (@accessory_2.price * 2)
     subtotal_1 = @accessory_1.price
-    grand_total = subtotal_12 + subtotal_1
-    expect(page).to have_content(@accessory_12.name)
-    expect(page).to have_content("Price: $#{@accessory_12.price}.00")
+    grand_total = subtotal_2 + subtotal_1
     expect(page).to have_content(@accessory_1.name)
     expect(page).to have_content("Price: $#{@accessory_1.price}.00")
-    expect(page).to have_content("Subtotal: $#{subtotal_12}.00")
     expect(page).to have_content("Subtotal: $#{subtotal_1}.00")
+    expect(page).to have_content(@accessory_2.name)
+    expect(page).to have_content("Price: $#{@accessory_2.price}.00")
+    expect(page).to have_content("Subtotal: $#{subtotal_2}.00")
     expect(page).to have_content("Total: $#{grand_total}.00")
     expect(page).to have_button("Checkout")
   end
 
-  xit "clicks on checkout and button and recieves message" do
+  it "clicks on checkout and button and recieves message" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
     visit bike_shop_path
@@ -46,7 +46,7 @@ describe "user visits their cart page" do
     end
 
     2.times do
-      within("#accessory-#{@accessory_12.id}") do
+      within("#accessory-#{@accessory_2.id}") do
         click_button "Add to Cart"
       end
     end
@@ -56,6 +56,13 @@ describe "user visits their cart page" do
     click_button "Checkout"
 
     expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content("Successfully submitted your order totalling $35.00")
+    expect(page).to have_content("Successfully submitted your order totalling $55.00")
+
+    visit '/cart'
+
+    expect(page).to_not have_content(@accessory_1.name)
+    expect(page).to_not have_content("Price: $#{@accessory_1.price}.00")
+    expect(page).to_not have_content(@accessory_2.name)
+    expect(page).to_not have_content("Price: $#{@accessory_2.price}.00")
   end
 end
