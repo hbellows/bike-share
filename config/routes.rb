fc1b35cd4e1c1273803d3b3754eefd69a4d371ee
abdyqew <aqudrat630@gmail,com>
2018-09-12 11:41:41 -0600

    fixes user auth test, sets root to bikeshop, deletes (previous root)dashboard view,controller,route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  root to: 'dashboard#index'
+  root to: 'accessories#index'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
