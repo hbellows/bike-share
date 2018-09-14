de980b1a60fa07dc31ea93ddc70937a0401f92cb
Harper Bellows <harper.bellows@gmail.com>
2018-09-13 20:26:27 -0600

    add admin station index untangle routes for new namespece guidance

--- spec/features/admin/stations/admin_stations_index_spec.rb
+++ spec/features/admin/stations/admin_stations_index_spec.rb
@@ -8,7 +8,7 @@ describe "Admin Stations Index" do
       station2 = create(:station)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-      visit admin_stations_path
+      visit stations_path
 
       expect(page).to have_content("Add Station")
 
