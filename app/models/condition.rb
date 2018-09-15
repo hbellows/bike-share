8b18e2b3ae0f0bd08d8f701d0d11cf8ec3a33890
abdyqew <aqudrat630@gmail,com>
2018-09-15 12:40:07 -0600

    finishes conditions dash methods/spec

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -8,25 +8,25 @@ class Condition < ApplicationRecord
   validates_presence_of :mean_wind_speed
   validates_presence_of :precipitation
 
-  def self.max_rides_high_temp_breakdown(min, max)
+  def self.max_rides_breakdown(min, max, attribute)
     Trip.select("start_date, count(trips.id) as count").
     joins("join conditions ON conditions.date=trips.start_date").
-    where("max_temperature between ? and ?", min, max).
+    where("#{attribute} between ? and ?", min, max).
     group(:start_date).
     order("count(trips.id) DESC").first.count
   end
 
-  def self.min_rides_high_temp_breakdown(min, max)
+  def self.min_rides_breakdown(min, max, attribute)
     Trip.select("start_date, count(trips.id) as count").
     joins("join conditions ON conditions.date=trips.start_date").
-    where("max_temperature between ? and ?", min, max).
+    where("#{attribute} between ? and ?", min, max).
     group(:start_date).
     order("count(trips.id) ASC").first.count
   end
 
-  def self.avg_rides_high_temp_breakdown(min, max)
-    trip_count = Trip.select("trips.*, COUNT(trips.id) as count").joins("JOIN conditions ON trips.start_date=conditions.date").where("max_temperature between ? and ?", min, max).group("trips.id")
-    trip_total = Trip.count
-    (trip_count / trip_total.to_f) * 100
+  def self.avg_rides_breakdown(min, max, attribute)
+    trip_count = Trip.select("start_date, count(trips.id) as count").joins("join conditions ON conditions.date=trips.start_date").where("#{attribute} between ? and ?", min, max).group(:start_date).length
+    trip_total = Trip.select("start_date, count(trips.id) as count").joins("join conditions ON conditions.date=trips.start_date").where("#{attribute} between ? and ?", min, max).group(:start_date).size.values.sum
+    trip_total / trip_count
   end
 end
--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Condition, type: :model do
     it { should validate_presence_of :precipitation }
   end
   describe "class methods" do
-    describe "high_temp_breakdown method" do
+    describe "high_temp_breakdown methods" do
       it "should return average/max/min rides at range of high temperature" do
         station_1, station_2 = create_list(:station, 2)
         create(:condition, max_temperature: 72, date: '2018-09-11 16:34:34')
@@ -21,9 +21,51 @@ RSpec.describe Condition, type: :model do
         create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-12 16:34:34', end_date: '2018-09-12 17:34:34')
         create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-13 16:34:34', end_date: '2018-09-13 17:34:34')
 
-        expect(Condition.max_rides_high_temp_breakdown(70,79.9)).to eq(8)
-        expect(Condition.min_rides_high_temp_breakdown(70,79.9)).to eq(4)
-        expect(Condition.avg_rides_high_temp_breakdown(70,79.9)).to eq(6)
+        expect(Condition.max_rides_breakdown(70,79.9,'max_temperature')).to eq(8)
+        expect(Condition.min_rides_breakdown(70,79.9,'max_temperature')).to eq(4)
+        expect(Condition.avg_rides_breakdown(70,79.9,'max_temperature')).to eq(6)
+      end
+    end
+    describe "precipitation_breakdown methods" do
+      it "should return average/max/min rides at range of precipitation" do
+        station_1, station_2 = create_list(:station, 2)
+        create(:condition, precipitation: 0.5, date: '2018-09-11 16:34:34')
+        create(:condition, precipitation: 0.7, date: '2018-09-12 16:34:34')
+        create_list(:trip, 4, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-11 16:34:34', end_date: '2018-09-11 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-12 16:34:34', end_date: '2018-09-12 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-13 16:34:34', end_date: '2018-09-13 17:34:34')
+
+        expect(Condition.max_rides_breakdown(0.5,1,'precipitation')).to eq(8)
+        expect(Condition.min_rides_breakdown(0.5,1,'precipitation')).to eq(4)
+        expect(Condition.avg_rides_breakdown(0.5,1,'precipitation')).to eq(6)
+      end
+    end
+    describe "precipitation_breakdown methods" do
+      it "should return average/max/min rides at range of mean_wind_speed" do
+        station_1, station_2 = create_list(:station, 2)
+        create(:condition, mean_wind_speed: 0, date: '2018-09-11 16:34:34')
+        create(:condition, mean_wind_speed: 4, date: '2018-09-12 16:34:34')
+        create_list(:trip, 4, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-11 16:34:34', end_date: '2018-09-11 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-12 16:34:34', end_date: '2018-09-12 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-13 16:34:34', end_date: '2018-09-13 17:34:34')
+
+        expect(Condition.max_rides_breakdown(0,4,'mean_wind_speed')).to eq(8)
+        expect(Condition.min_rides_breakdown(0,4,'mean_wind_speed')).to eq(4)
+        expect(Condition.avg_rides_breakdown(0,4,'mean_wind_speed')).to eq(6)
+      end
+    end
+    describe "precipitation_breakdown methods" do
+      it "should return average/max/min rides at range of mean_visibility" do
+        station_1, station_2 = create_list(:station, 2)
+        create(:condition, mean_visibility: 0, date: '2018-09-11 16:34:34')
+        create(:condition, mean_visibility: 4, date: '2018-09-12 16:34:34')
+        create_list(:trip, 4, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-11 16:34:34', end_date: '2018-09-11 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-12 16:34:34', end_date: '2018-09-12 17:34:34')
+        create_list(:trip, 8, start_station_id: station_1.id, end_station_id: station_2.id, start_date: '2018-09-13 16:34:34', end_date: '2018-09-13 17:34:34')
+
+        expect(Condition.max_rides_breakdown(0,4,'mean_visibility')).to eq(8)
+        expect(Condition.min_rides_breakdown(0,4,'mean_visibility')).to eq(4)
+        expect(Condition.avg_rides_breakdown(0,4,'mean_visibility')).to eq(6)
       end
     end
   end
