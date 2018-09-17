6d4c88a2da65a867179ef70cac6c4c3315f4343e
Mike <mikecm@gmail.com>
2018-09-17 15:36:17 -0600

    Fixes tests that styling broke

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     if current_admin?
-      @user = User.find(current_user.id)
+      @user = User.find(params[:id])
     elsif current_user
       @user = User.find(current_user.id)
     end
